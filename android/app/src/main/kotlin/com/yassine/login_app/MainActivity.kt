20cfe21877cf0faba03db6d5389304ca28c51c3d
Yassine-ch <yassine.chebbi2023@gmail.com>
2022-04-09 00:26:50 +0100

    login

---
+++ android/app/src/main/kotlin/com/yassine/login_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yassine.login_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
