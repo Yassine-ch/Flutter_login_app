7b25152727e7e032a6bbca388ba26428781cf0b6
Yassine-ch <yassine.chebbi2023@gmail.com>
2022-04-09 02:02:40 +0100

    login

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -15,12 +17,86 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Stack(
             children: [
               Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xff65b0bb),
+                      Color(0xff5a9ea8),
+                      Color(0xff508c95),
+                      Color(0xff467b82),
+                      Color(0xff3c6970),
+                      Color(0xff32585d),
+                      Color(0xff28464a),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  child: Column(
 
-              )
+                    children: [
+                      SizedBox(height: 100),
+                      Text(
+                        "Hello USer",
+                        style: TextStyle(color: Colors.white, fontSize: 35),
+                      ),
+                      SizedBox(height: 100),
+                      buildEmail(),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Email",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold)),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60,
+          decoration: BoxDecoration(
+              color: Color(0xffebefff),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(0, 2),
+                )
+              ]),
+          child: TextField(
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Color(0xff4c5166),
+                ),
+                hintText: "Email",
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                )),
+          ),
+        )
+      ],
     );
   }
 }
