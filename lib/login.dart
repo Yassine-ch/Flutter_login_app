af747b0b3a175cb086ccba8cf36a14fb39207670
Yassine-ch <yassine.chebbi2023@gmail.com>
2022-04-10 01:14:15 +0100

    login

--- lib/login.dart
+++ lib/login.dart
@@ -8,6 +8,17 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool remember = false;
+  bool sec = true;
+  var visable = Icon(
+    Icons.visibility,
+    color: Color(0xff4c51166),
+  );
+  var visableoff = Icon(
+    Icons.visibility_off,
+    color: Color(0xff4c51166),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +48,6 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: Padding(
                   padding: const EdgeInsets.only(right: 20, left: 20),
                   child: Column(
-
                     children: [
                       SizedBox(height: 100),
                       Text(
@@ -46,6 +56,14 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                       SizedBox(height: 100),
                       buildEmail(),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      buildPassword(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      buildRememberpwd(),
                     ],
                   ),
                 ),
@@ -82,6 +100,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 )
               ]),
           child: TextField(
+            keyboardType: TextInputType.emailAddress,
             style: TextStyle(color: Colors.black),
             decoration: InputDecoration(
                 border: InputBorder.none,
@@ -97,6 +116,86 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         )
       ],
+    );
+  }
+
+  Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Password",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold)),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60,
+          decoration: BoxDecoration(
+              color: Color(0xffebefff),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(0, 2),
+                )
+              ]),
+          child: TextField(
+            obscureText: sec,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                prefixIcon: Icon(
+                  Icons.vpn_key,
+                  color: Color(0xff4c5166),
+                ),
+                hintText: "Password",
+                suffix: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      sec = !sec;
+                    });
+                  },
+                  icon: sec ? visableoff : visable,
+                ),
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                )),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildRememberpwd() {
+    return Container(
+      height: 60,
+      child: Row(
+        children: [
+          Theme(
+              data: ThemeData(
+                unselectedWidgetColor: Colors.white,
+              ),
+              child: Checkbox(
+                value: remember,
+                checkColor: Colors.blueGrey,
+                activeColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value;
+                  });
+                },
+              )),
+          Text(
+            "Remember me",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ],
+      ),
     );
   }
 }
